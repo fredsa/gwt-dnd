b087f8defb55f30910cd389aeefd391196d34811
Fred Sauer <fredsa@gmail.com>
2007-01-27 17:56:12 +0000

    Added DropController interface, implemented by AbstractDropController
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@38 c7e6bd1f-9827-0410-b8b9-973c5d279602

---
+++ DragDrop/src/com/allen_sauer/gwt/dragdrop/client/drop/DropController.java
@@ -0,0 +1,26 @@
+package com.allen_sauer.gwt.dragdrop.client.drop;
+
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+
+import com.allen_sauer.gwt.dragdrop.client.DragAndDropController;
+
+public interface DropController {
+
+  public abstract Panel getDropTargetPanel();
+
+  public abstract String getDropTargetStyleName();
+
+  public abstract void onDrop(DragAndDropController dragAndDropController,
+      Widget draggable);
+
+  public abstract void onPreDropEnter(
+      DragAndDropController dragAndDropController, Widget draggable);
+
+  public abstract void onPreDropLeave(
+      DragAndDropController dragAndDropController, Widget draggable);
+
+  public abstract void onPreDropMove(
+      DragAndDropController dragAndDropController, Widget draggable);
+
+}
