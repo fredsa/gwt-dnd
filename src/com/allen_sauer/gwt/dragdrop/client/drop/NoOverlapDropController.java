88d460469632e043654f4d876cc438cdbd819fb4
Fred Sauer <fredsa@gmail.com>
2007-01-29 20:18:28 +0000

    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@45 c7e6bd1f-9827-0410-b8b9-973c5d279602

---
+++ src/com/allen_sauer/gwt/dragdrop/client/drop/NoOverlapDropController.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2006 Fred Sauer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.allen_sauer.gwt.dragdrop.client.drop;
+
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import com.allen_sauer.gwt.dragdrop.client.DragAndDropController;
+import com.allen_sauer.gwt.dragdrop.client.util.Area;
+import com.allen_sauer.gwt.dragdrop.client.util.Location;
+
+import java.util.Iterator;
+
+/**
+ * A {@link com.allen_sauer.gwt.dragdrop.demo.client.drop.DropController} which
+ * constrains prevents draggable widgets from overlapping.
+ * 
+ * TODO handle drop() and onDrop() with overlapping coordinates properly
+ */
+public class NoOverlapDropController extends AbsolutePositionDropController {
+
+  private AbsolutePanel dropTargetPanel;
+
+  public NoOverlapDropController(AbsolutePanel dropTargetPanel) {
+    super(dropTargetPanel);
+    this.dropTargetPanel = dropTargetPanel;
+  }
+
+  public String getDropTargetStyleName() {
+    return "dragdrop-drop-target dragdrop-no-overlap-drop-target";
+  }
+
+  private boolean collision(DragAndDropController dragAndDropController, Widget widget, Area area) {
+    for (Iterator iterator = this.dropTargetPanel.iterator(); iterator.hasNext();) {
+      Widget w = (Widget) iterator.next();
+      if ((w == widget) || (w == dragAndDropController.getPostioningBox())) {
+        continue;
+      }
+      if ((new Area(w, this.dropTargetPanel)).intersects(area)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // TODO attempt to move as close to desired location as possible rather than
+  // simply try current desired location
+  protected void constrainedWidgetMove(DragAndDropController dragAndDropController, Widget widget) {
+    AbsolutePanel boundryPanel = dragAndDropController.getBoundryPanel();
+    Area dropArea = new Area(this.dropTargetPanel, boundryPanel);
+    Area widgetArea = new Area(widget, boundryPanel);
+    Location location = new Location(dragAndDropController.getDraggable(), this.dropTargetPanel);
+    location.constrain(0, 0, dropArea.getWidth() - widgetArea.getWidth(), dropArea.getHeight() - widgetArea.getHeight());
+    widgetArea.moveTo(location);
+    if (!collision(dragAndDropController, dragAndDropController.getDraggable(), widgetArea)) {
+      this.dropTargetPanel.add(widget, location.getLeft(), location.getTop());
+    }
+  }
+
+}
