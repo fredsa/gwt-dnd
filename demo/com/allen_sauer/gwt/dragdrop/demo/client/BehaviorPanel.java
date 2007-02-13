7a6e1151dcd2528cc9c122d08a05b6d106069d2c
Fred Sauer <fredsa@gmail.com>
2007-02-13 20:20:46 +0000

    visual cleanup of behavior radio button panel
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@82 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- demo/com/allen_sauer/gwt/dragdrop/demo/client/BehaviorPanel.java
+++ demo/com/allen_sauer/gwt/dragdrop/demo/client/BehaviorPanel.java
@@ -3,6 +3,7 @@ package com.allen_sauer.gwt.dragdrop.demo.client;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -12,15 +13,16 @@ import com.allen_sauer.gwt.dragdrop.client.DragController;
  * Allows the user to select the desired drag behavior
  * for this demonstration.
  */
-public class BehaviorPanel extends VerticalPanel {
+public class BehaviorPanel extends SimplePanel {
 
   public BehaviorPanel(final DragController dragController) {
+    VerticalPanel verticalPanel = new VerticalPanel();
     final RadioButton classicButton = newButton("Classic", "widget is directly draggable");
     final RadioButton proxyButton = newButton("Proxy", "a separate proxy widget is dragged");
 
-    add(new Label("You may select your preferred drag behavior..."));
-    add(classicButton);
-    add(proxyButton);
+    verticalPanel.add(new Label("You may select your preferred drag behavior..."));
+    verticalPanel.add(classicButton);
+    verticalPanel.add(proxyButton);
 
     if (dragController.isDragProxyEnabled()) {
       proxyButton.setChecked(true);
@@ -36,6 +38,8 @@ public class BehaviorPanel extends VerticalPanel {
 
     classicButton.addClickListener(listener);
     proxyButton.addClickListener(listener);
+    
+    setWidget(verticalPanel);
   }
 
   protected void onLoad() {
