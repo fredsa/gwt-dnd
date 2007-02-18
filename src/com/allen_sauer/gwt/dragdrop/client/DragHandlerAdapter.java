6a7efc7569d01c6e996adb35abfb60963789a097
Fred Sauer <fredsa@gmail.com>
2007-02-18 06:21:16 +0000

    Add convience adapter class
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@95 c7e6bd1f-9827-0410-b8b9-973c5d279602

---
+++ src/com/allen_sauer/gwt/dragdrop/client/DragHandlerAdapter.java
@@ -0,0 +1,21 @@
+package com.allen_sauer.gwt.dragdrop.client;
+
+/**
+ * Convenience class for anonymous inner classes not
+ * wishing to define all methods required by this interface.
+ */
+public class DragHandlerAdapter implements DragHandler {
+
+  public void onDragEnd(DragEndEvent event) {
+  }
+
+  public void onDragStart(DragStartEvent event) {
+  }
+
+  public void onPreviewDragEnd(DragEndEvent event) throws VetoDragException {
+  }
+
+  public void onPreviewDragStart(DragStartEvent event) throws VetoDragException {
+  }
+
+}
