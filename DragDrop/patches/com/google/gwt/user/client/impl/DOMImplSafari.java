0112bb0ca32386c7bd773d23ff5e1538627e41b3
Fred Sauer <fredsa@gmail.com>
2007-02-23 23:59:52 +0000

    Fix for Safari when elements are not attached causing widget.getAbsoluteLeft/Top to return NaN
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@113 c7e6bd1f-9827-0410-b8b9-973c5d279602

---
+++ DragDrop/patches/com/google/gwt/user/client/impl/DOMImplSafari.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2006 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.user.client.impl;
+
+import com.google.gwt.user.client.Element;
+
+/**
+ * Safari implementation of {@link com.google.gwt.user.client.impl.DomImpl}.
+ */
+class DOMImplSafari extends DOMImplStandard {
+
+  public native int getAbsoluteLeft(Element elem) /*-{
+    var left = 0;
+    while (elem) {
+      // Unattached elements and elements (or their ancestors) with style
+      // 'display: none' have no offsetLeft
+      if (elem.offsetLeft != null) {
+        left += elem.offsetLeft - elem.scrollLeft;
+      }
+
+      // Safari bug: a top-level absolutely positioned element includes the
+      // body's offset position already.
+      var parent = elem.offsetParent;
+      if (parent && (parent.tagName == 'BODY') &&
+          (elem.style.position == 'absolute')) {
+        return left;
+      }
+      
+      elem = parent;
+    }
+    return left + $doc.body.scrollLeft;
+  }-*/;
+
+  public native int getAbsoluteTop(Element elem) /*-{
+    var top = 0;
+    while (elem) {
+      // Unattached elements and elements (or their ancestors) with style
+      // 'display: none' have no offsetTop
+      if (elem.offsetTop != null) {
+        top += elem.offsetTop - elem.scrollTop;
+      }
+
+      // Safari bug: a top-level absolutely positioned element includes the
+      // body's offset position already.
+      var parent = elem.offsetParent;
+      if (parent && (parent.tagName == 'BODY') &&
+          (elem.style.position == 'absolute')) {
+        return top;
+      }
+      
+      elem = parent;
+    }
+    return top + $doc.body.scrollTop;
+  }-*/;
+}
