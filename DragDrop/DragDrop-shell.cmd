70e07ab9ae83297aa56728215d5a5e913706ed1e
Fred Sauer <fredsa@gmail.com>
2007-05-29 19:54:13 +0000

    trying approach where project file system path matches desired patch path
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@212 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/DragDrop-shell.cmd
+++
@@ -1,1 +0,0 @@
-@java -cp "%~dp0\src;%~dp0\demo;%~dp0\example;%~dp0\bin;C:/fred/java/lib/gwt-log.jar;C:/home/fsauer/gwt/trunk/build/staging/gwt-windows-0.0.0/gwt-user.jar;C:/home/fsauer/gwt/trunk/build/staging/gwt-windows-0.0.0/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www\demo" %* com.allen_sauer.gwt.dragdrop.demo.DragDropDemo/DragDropDemo.html
