816e9b0ad8b09215d972d3e4f4191bdd8b6174ad
Fred Sauer <fredsa@gmail.com>
2007-08-17 02:03:05 +0000

    Finishing up use deferred binding from r278.
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@281 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- src/com/allen_sauer/gwt/dragdrop/client/util/impl/DOMUtilImplIE6.java
+++ src/com/allen_sauer/gwt/dragdrop/client/util/impl/DOMUtilImplIE6.java
@@ -21,102 +21,30 @@ import com.google.gwt.user.client.ui.Widget;
 public class DOMUtilImplIE6 extends DOMUtilImpl {
   public native int getBorderLeft(Element elem)
   /*-{
-    try {
-      // Compare to 'null' since 'undefined' not always JavaScript keyword
-      if (elem.clientLeft != null) {
-        return elem.clientLeft;
-      } else if ($doc.defaultView != null) {
-        var computedStyle = $doc.defaultView.getComputedStyle(elem, null);
-        if (computedStyle != null) {
-          var borderLeftWidth = computedStyle.getPropertyValue("border-left-width");
-          return borderLeftWidth.indexOf("px") == -1 ? 0 : parseInt(borderLeftWidth.substr(0, borderLeftWidth.length - 2));
-        } else {
-          // Handle Safari when elem is hidden
-          return 0;
-        }
-      } else {
-        throw new Error("Unable to determine border-left-width");
-      }
-    } catch(e) {
-      throw new Error("JavaScript Exception caught by DOMUtil.getBorderLeft():\n" + e);
-    }
+    return elem.clientLeft;
   }-*/;
 
   public native int getBorderTop(Element elem)
   /*-{
-    try {
-      // Compare to 'null' since 'undefined' not always JavaScript keyword
-      if (elem.clientTop != null) {
-        return elem.clientTop;
-      } else if ($doc.defaultView != null) {
-        var computedStyle = $doc.defaultView.getComputedStyle(elem, null);
-        if (computedStyle != null) {
-          var borderTopWidth = computedStyle.getPropertyValue("border-top-width");
-          return borderTopWidth.indexOf("px") == -1 ? 0 : parseInt(borderTopWidth.substr(0, borderTopWidth.length - 2));
-        } else {
-          // Handle Safari when elem is hidden
-          return 0;
-        }
-      } else {
-        throw new Error("Unable to determine border-top-width");
-      }
-    } catch(e) {
-      throw new Error("JavaScript Exception caught by DOMUtil.getBorderTop():\n" + e);
-    }
+    return elem.clientTop;
   }-*/;
 
   public native int getClientHeight(Element elem)
   /*-{
-    try{
-      // Compare to null since undefined not always JavaScript keyword
-      if (elem.clientHeight != null) {
-        return elem.clientHeight;
-      } else {
-        // Safari when element is hidden
-        return 0;
-      }
-    } catch(e) {
-      throw new Error("JavaScript Exception caught by DOMUtil.getClientHeight():\n" + e);
-    }
+    return elem.clientHeight;
   }-*/;
 
   public native int getClientWidth(Element elem)
   /*-{
-    try{
-      // Compare to null since undefined not always JavaScript keyword
-      if (elem.clientWidth != null) {
-        return elem.clientWidth;
-      } else {
-        // Safari when element is hidden
-        return 0;
-      }
-    } catch(e) {
-      throw new Error("JavaScript Exception caught by DOMUtil.getClientWidth():\n" + e);
-    }
+    return elem.clientWidth;
   }-*/;
 
   public int getHorizontalBorders(Widget widget) {
     return widget.getOffsetWidth() - getClientWidth(widget.getElement());
   }
 
-  public native String getNodeName(Element element)
-  /*-{
-    return element.nodeName;
-  }-*/;
-
-  public int getVerticalBorders(Widget widget) {
-    return widget.getOffsetHeight() - getClientHeight(widget.getElement());
-  }
-
-  public native void preventSelection(Element elem)
-  /*-{
-    elem.onselectstart = function() { return false; }
-  }-*/;
-
   public native void unselect()
   /*-{
-    // $doc.execCommand('Unselect', false, null);
     $doc.selection.empty();
-    // $wnd.getSelection().removeAllRanges();
   }-*/;
 }
