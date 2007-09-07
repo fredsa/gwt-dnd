c2d33b0af69921cd689421fc31ccd31d4868c484
Fred Sauer <fredsa@gmail.com>
2007-09-07 02:03:51 +0000

    static UncaughtExceptionHandler
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@329 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- getting-started/com/mycompany/client/MyApplication.java
+++ getting-started/com/mycompany/client/MyApplication.java
@@ -14,18 +14,17 @@ import com.allen_sauer.gwt.dragdrop.client.drop.AbsolutePositionDropController;
 import com.allen_sauer.gwt.dragdrop.client.drop.DropController;
 
 public class MyApplication implements EntryPoint {
-
-  private static class MyUncaughtExceptionHandler implements UncaughtExceptionHandler {
-
-    public void onUncaughtException(Throwable ex) {
-      Window.alert("Uncaught Exception\n" + (ex == null ? "null" : ex.toString()));
-    }
+  static {
+    // catch unexpected exceptions, especially in other browsers
+    // set the handler statically so that it is in effect for onModuleLoad()
+    GWT.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+      public void onUncaughtException(Throwable ex) {
+        Window.alert("Uncaught Exception\n" + (ex == null ? "null" : ex.toString()));
+      }
+    });
   }
 
   public void onModuleLoad() {
-    // Make sure we catch unexpected exceptions in web mode, especially in other browsers
-    GWT.setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
-
     AbsolutePanel boundaryPanel = new AbsolutePanel();
     boundaryPanel.setPixelSize(400, 300);
     boundaryPanel.addStyleName("getting-started-blue");
@@ -37,24 +36,34 @@ public class MyApplication implements EntryPoint {
     RootPanel.get().add(boundaryPanel);
     boundaryPanel.add(targetPanel, 10, 10);
 
-    // Create a DragController for each logical area where a set of draggable
+    // create a DragController for each logical area where a set of draggable
     // widgets and drop targets will be allowed to interact with one another.
     DragController dragController = new PickupDragController(boundaryPanel, true);
 
-    // Create a DropController for each drop target on which draggable widgets can be dropped
+    // create a DropController for each drop target on which draggable widgets
+    // can be dropped
     DropController dropController = new AbsolutePositionDropController(targetPanel);
 
     // Don't forget to register each DropController with a DragController
     dragController.registerDropController(dropController);
 
+    // create a few randomly placed draggable labels
     for (int i = 1; i <= 5; i++) {
+      // create a label and give it style
       Label label = new Label("Label #" + i, false);
       label.addStyleName("getting-started-label");
+      
+      // add it to the DOM so that offset width/height becomes available
       targetPanel.add(label, 0, 0);
+      
+      // determine random label location within target pael
       int left = (int) (Math.random() * (targetPanel.getOffsetWidth() - label.getOffsetWidth()));
       int top = (int) (Math.random() * (targetPanel.getOffsetHeight() - label.getOffsetHeight()));
+
+      // move the label
       targetPanel.setWidgetPosition(label, left, top);
-      // Make individual widgets draggable
+      
+      // make the label draggable
       dragController.makeDraggable(label);
     }
   }
