e0dbc887fe9480bacabdb57b32a4769cf1222fe0
Fred Sauer <fredsa@gmail.com>
2007-11-09 23:01:38 +0000

    style pretty compilation for better debugging in web mode online demo
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@487 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop-Demo-shell.cmd
+++ DragDrop-Demo-shell.cmd
@@ -1,1 +1,1 @@
-@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT_TRUNK%/user/src;%GWT_PLATFORM%/gwt-user.jar;%GWT_PLATFORM%/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.allen_sauer.gwt.dragdrop.demo.DragDropDemo/DragDropDemo.html
+@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT_TRUNK%/user/src;%GWT_PLATFORM%/gwt-user.jar;%GWT_PLATFORM%/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.allen_sauer.gwt.dragdrop.demo.DragDropDemo/DragDropDemo.html -style PRETTY
