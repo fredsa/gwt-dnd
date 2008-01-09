6f6cd87bb63d5bd6815ad92b49c5cfa2fd7658f3
Fred Sauer <fredsa@gmail.com>
2008-01-09 18:11:44 +0000

    Prevent occasional "Unknown runtime error" in IE at start of drag
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@645 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- src/com/allen_sauer/gwt/dnd/client/util/impl/DOMUtilImplIE6.java
+++ src/com/allen_sauer/gwt/dnd/client/util/impl/DOMUtilImplIE6.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Fred Sauer
+ * Copyright 2008 Fred Sauer
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -24,7 +24,11 @@ import com.google.gwt.user.client.Element;
 public class DOMUtilImplIE6 extends DOMUtilImpl {
   public native void cancelAllDocumentSelections()
   /*-{
-    $doc.selection.empty();
+    try {
+      $doc.selection.empty();
+    } catch(e) {
+      // ignore 'Unknown runtime error'
+    }
   }-*/;
 
   public native int getBorderLeft(Element elem)
