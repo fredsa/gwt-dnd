6aba01bda48b6a242c445025c558198986bf29d1
Fred Sauer <fredsa@gmail.com>
2008-01-19 22:40:18 +0000

    for posterity
    git-svn-id: https://gwt-dnd.googlecode.com/svn/tags/gwt-dnd-2.0.7-r666@667 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- src/com/allen_sauer/gwt/dnd/client/PickupDragController.java
+++ src/com/allen_sauer/gwt/dnd/client/PickupDragController.java
@@ -45,6 +45,7 @@ import java.util.Iterator;
 public class PickupDragController extends AbstractDragController {
   private static class SavedWidgetInfo {
     int initialDraggableIndex;
+    String initialDraggableMargin;
     Widget initialDraggableParent;
     Location initialDraggableParentLocation;
   }
@@ -371,13 +372,15 @@ public class PickupDragController extends AbstractDragController {
 
   /**
    * Restore the selected widgets with their original style.
-   * 
-   * @deprecated This method may be removed in the future
-   * 
    * @see #saveSelectedWidgetsLocationAndStyle()
    * @see #restoreSelectedWidgetsLocation()
    */
   protected void restoreSelectedWidgetsStyle() {
+    for (Iterator iterator = context.selectedWidgets.iterator(); iterator.hasNext();) {
+      Widget widget = (Widget) iterator.next();
+      SavedWidgetInfo info = (SavedWidgetInfo) savedWidgetInfoMap.get(widget);
+      DOM.setStyleAttribute(widget.getElement(), "margin", info.initialDraggableMargin);
+    }
   }
 
   /**
@@ -412,6 +415,9 @@ public class PickupDragController extends AbstractDragController {
                 + GWT.getTypeName(info.initialDraggableParent)
                 + "'; Please create your own DragController and override saveDraggableLocationAndStyle() and restoreDraggableLocation()");
       }
+
+      info.initialDraggableMargin = DOM.getStyleAttribute(widget.getElement(), "margin");
+      DOM.setStyleAttribute(widget.getElement(), "margin", "0px");
       savedWidgetInfoMap.put(widget, info);
     }
   }
