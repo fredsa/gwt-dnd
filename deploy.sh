3d269ea505839a6ef87a839a3821371538e09d48
Fred Sauer <fredsa@gmail.com>
2008-03-09 02:50:00 +0000

    begin migration to GWT 1.5 / Java 5
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@699 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 set -uev
 rm -rf .gwt-cache www tomcat
 ./*-compile.cmd
-scp -r www/* sauer@allen-sauer.com:
-ssh sauer@allen-sauer.com ./deploy.sh $( cd www; ls -1 )
+mod=$( ls www )
+scp -r www/$mod/std sauer@allen-sauer.com:$mod
+ssh sauer@allen-sauer.com ./deploy.sh $mod
