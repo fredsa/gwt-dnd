a265e93fd980a3ee386accec26da3cef823f498d
Fred Sauer <fredsa@gmail.com>
2008-05-07 06:09:00 +0000

    Fix for gwt-dnd issue 24
    Scroll Panels (actually any offset parent elements are now taken into account when considering drop target intersection with the mouse cursor.
    
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@720 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/src/com/allen_sauer/gwt/dnd/client/util/DOMUtil.java
+++ DragDrop/src/com/allen_sauer/gwt/dnd/client/util/DOMUtil.java
@@ -16,8 +16,8 @@
 package com.allen_sauer.gwt.dnd.client.util;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.IndexedPanel;
 import com.google.gwt.user.client.ui.Widget;
 
