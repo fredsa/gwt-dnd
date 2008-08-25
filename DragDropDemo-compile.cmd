5a540020c12864898b8dc88db91eea6af9bcdad1
Fred Sauer <fredsa@gmail.com>
2008-08-25 02:52:56 +0000

    update launch configs, compile/shell scripts, eclipse project/classpath
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@797 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDropDemo-compile.cmd
+++ DragDropDemo-compile.cmd
@@ -1,1 +1,1 @@
-@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT15_HOME%/gwt-user.jar;%GWT15_HOME%/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.allen_sauer.gwt.dnd.demo.DragDropDemo -style PRETTY
+@java -Xmx1024m -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT_HOME%/gwt-user.jar;%GWT_HOME%/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.allen_sauer.gwt.dnd.demo.DragDropDemo -style PRETTY
