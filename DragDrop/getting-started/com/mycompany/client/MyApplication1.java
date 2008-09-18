9c5dae6cd248da86fbc86676d788af90af1fa463
Fred Sauer <fredsa@gmail.com>
2008-09-18 14:34:11 +0000

    Undo accidental commits of developer test cases
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@833 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/getting-started/com/mycompany/client/MyApplication1.java
+++ DragDrop/getting-started/com/mycompany/client/MyApplication1.java
@@ -16,87 +16,30 @@
 package com.mycompany.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
-import com.allen_sauer.gwt.dnd.client.DragContext;
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
-import com.allen_sauer.gwt.dnd.client.drop.SimpleDropController;
 
 /**
  * Illustrative example.
  */
 public class MyApplication1 implements EntryPoint {
-  public class IconDropController extends SimpleDropController {
 
-    public IconDropController(Panel panel) {
-      super(panel);
-    }
-
-    @Override
-    public void onDrop(DragContext context) {
-      super.onDrop(context);
-      Widget draggable = context.draggable;
-      int top = context.desiredDraggableY - context.boundaryPanel.getAbsoluteTop();
-      int left = context.desiredDraggableX - context.boundaryPanel.getAbsoluteLeft();
-      draggable.getElement().getStyle().setProperty("top", top + "px");
-      draggable.getElement().getStyle().setProperty("left", left + "px");
-    }
-  }
-
+  /**
+   * Main entry point method.
+   */
   public void onModuleLoad() {
-    ScrollPanel scrP = new ScrollPanel();
-    scrP.setSize("400px", "600px");
-    RootPanel.get().add(scrP);
-    VerticalPanel vp = new VerticalPanel();
-    scrP.add(vp);
-    AbsolutePanel fp1 = new AbsolutePanel();
-    fp1.setSize("350px", "500px");
-    fp1.getElement().getStyle().setProperty("border", "solid 2px black");
-    AbsolutePanel fp2 = new AbsolutePanel();
-    fp2.setSize("350px", "500px");
-    fp2.getElement().getStyle().setProperty("border", "solid 2px black");
-    AbsolutePanel fp3 = new AbsolutePanel();
-    fp3.setSize("350px", "500px");
-    fp3.getElement().getStyle().setProperty("border", "solid 2px black");
-    AbsolutePanel fp4 = new AbsolutePanel();
-    fp4.setSize("350px", "500px");
-    fp4.getElement().getStyle().setProperty("border", "solid 2px black");
-    vp.add(fp1);
-    vp.add(fp2);
-    vp.add(fp3);
-    vp.add(fp4);
-    Image l1 = new Image();
-    l1.setSize("40px", "40px");
-    l1.getElement().getStyle().setProperty("position", "absolute");
-    l1.getElement().getStyle().setProperty("background", "red");
-    Image l2 = new Image();
-    l2.setSize("40px", "40px");
-    l2.getElement().getStyle().setProperty("position", "absolute");
-    l2.getElement().getStyle().setProperty("background", "lime");
-    fp1.add(l2);
-    fp4.add(l1);
+    // ensure the document BODY has dimensions in standards mode
+    RootPanel.get().setPixelSize(600, 600);
 
-    // setup dragging
-    PickupDragController dragController = new PickupDragController(fp1, true);
-    dragController.setBehaviorDragProxy(true);
-    dragController.setBehaviorMultipleSelection(false);
-    dragController.setBehaviorConstrainedToBoundaryPanel(true);
-    dragController.makeDraggable(l2);
-    IconDropController dropController = new IconDropController(fp1);
-    dragController.registerDropController(dropController);
-
-    PickupDragController dragController2 = new PickupDragController(fp4, true);
-    dragController2.setBehaviorDragProxy(true);
-    dragController2.setBehaviorMultipleSelection(false);
-    dragController2.setBehaviorConstrainedToBoundaryPanel(true);
-    dragController2.makeDraggable(l1);
-    IconDropController dropController2 = new IconDropController(fp4);
-    dragController2.registerDropController(dropController2);
+    // create a DragController to manage drag-n-drop actions
+    // note: This creates an implicit DropController for the boundary panel
+    PickupDragController dragController = new PickupDragController(RootPanel.get(), true);
+    
+    // add a new image to the boundary panel and make it draggable
+    Image img = new Image("http://code.google.com/webtoolkit/logo-185x175.png");
+    RootPanel.get().add(img, 40, 30);
+    dragController.makeDraggable(img);
   }
 }
