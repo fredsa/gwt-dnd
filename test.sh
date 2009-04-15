531cc99d4d40024ac65403174becb9ea51e734b1
Fred Sauer <fredsa@gmail.com>
2009-04-15 23:29:05 +0000

    refactor project for new eclipse plugin
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk/DragDrop@853 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-set -uev
-rm -rf .gwt-cache www tomcat
-./*-compile.cmd
-mv www/com.allen_sauer.gwt.dnd.demo.DragDropDemo www/dnd
-scp -r www/* sauer@allen-sauer.com:
-ssh sauer@allen-sauer.com ./deploy.sh dnd
