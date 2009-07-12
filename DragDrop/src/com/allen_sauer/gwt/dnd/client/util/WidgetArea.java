42409fc14470ae8af078b20af0775ecadb438171
Fred Sauer <fredsa@gmail.com>
2009-07-12 02:35:39 +0000

    Fix for widgets which have a non-zero offsetLeft or offsetTop (due to another widget with the same offset parent) with a scrolling offset parent
    Found by: Pierre-Emmanuel
    
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@926 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/src/com/allen_sauer/gwt/dnd/client/util/WidgetArea.java
+++ DragDrop/src/com/allen_sauer/gwt/dnd/client/util/WidgetArea.java
@@ -38,50 +38,29 @@ public class WidgetArea extends AbstractArea {
     Element p;
 
     while (elem != null && (p = elem.getOffsetParent()) != null) {
-      setLeft(getLeft() + elem.getScrollLeft());
-      setTop(getTop() + elem.getScrollTop());
+      int left = elem.getAbsoluteLeft();
 
-      int temp;
-      if ((temp = getWidth() - DOMUtil.getClientWidth(elem)) > 0) {
-        setRight(getRight() - temp);
+      if (getLeft() < left) {
+        setLeft(left);
       }
-      if ((temp = getHeight() - DOMUtil.getClientHeight(elem)) > 0) {
-        setBottom(getBottom() - temp);
+
+      int top = elem.getAbsoluteTop();
+      if (getTop() < top) {
+        setTop(top);
+      }
+
+      int bottom = elem.getAbsoluteBottom();
+      if (getBottom() > bottom) {
+        setBottom(bottom);
+      }
+
+      int right = elem.getAbsoluteRight();
+      if (getRight() > right) {
+        setRight(right);
       }
 
       elem = p;
     }
   }
 
-  /**
-   * Determine the area of a widget relative to a panel. The area returned is such that the
-   * following are true:
-   * <ul>
-   * <li><code>parent.add(widget, area.getLeft(), area.getTop())</code> leaves the object in the
-   * exact same location on the screen</li>
-   * <li>
-   * <code>area.getRight() = area.getLeft() + widget.getOffsetWidget()</code></li>
-   * <li>
-   * <code>area.getBottom() = area.getTop() + widget.getOffsetHeight()</code></li>
-   * </ul>
-   * 
-   * Note that boundaryPanel need not be the parent node, or even an ancestor of widget. Therefore
-   * coordinates returned may be negative or may exceed the dimensions of boundaryPanel.
-   * 
-   * @param widget the widget whose area we seek
-   * @param reference the widget relative to which we seek our area. If <code>null</code>, then
-   *          <code>RootPanel().get()</code> is assumed
-   */
-  // public WidgetArea(Widget widget, Widget reference) {
-  // setLeft(widget.getAbsoluteLeft());
-  // setTop(widget.getAbsoluteTop());
-  //
-  // if (reference != null) {
-  // setLeft(getLeft() - reference.getAbsoluteLeft()
-  // - DOMUtil.getBorderLeft(reference.getElement()));
-  // setTop(getTop() - reference.getAbsoluteTop() - DOMUtil.getBorderTop(reference.getElement()));
-  // }
-  // setRight(getLeft() + widget.getOffsetWidth());
-  // setBottom(getTop() + widget.getOffsetHeight());
-  // }
 }
