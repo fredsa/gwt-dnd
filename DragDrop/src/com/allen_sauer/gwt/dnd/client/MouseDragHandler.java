ffcc38df7ce6bfc5ea201b79fb863f2aac337cdc
Fred Sauer <fredsa@google.com>
2010-11-04 17:33:58 +0000

    @SuppressWarnings
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@1053 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/src/com/allen_sauer/gwt/dnd/client/MouseDragHandler.java
+++ DragDrop/src/com/allen_sauer/gwt/dnd/client/MouseDragHandler.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2009 Fred Sauer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -465,6 +465,7 @@ class MouseDragHandler implements MouseMoveHandler, MouseDownHandler, MouseUpHan
     style.setBackgroundColor("blue");
   }
 
+  @SuppressWarnings("unchecked")
   private void onTouchEndorCancel(TouchEvent event) {
     if (event.getTouches().length() != 0) {
       // ignore multiple fingers for now
