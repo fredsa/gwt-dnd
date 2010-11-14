4e9e269fff103223cd30293459bb85f44f93ac24
Fred Sauer <fredsa@google.com>
2010-11-14 21:18:46 +0000

    remove unnecessary import
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@1055 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/src/com/allen_sauer/gwt/dnd/client/MouseDragHandler.java
+++ DragDrop/src/com/allen_sauer/gwt/dnd/client/MouseDragHandler.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2009 Fred Sauer
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -29,7 +29,6 @@ import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -267,8 +266,7 @@ class MouseDragHandler implements MouseMoveHandler, MouseDownHandler, MouseUpHan
           ((HasMouseDownHandlers) dragHandle).addMouseDownHandler(this));
       dragHandleMap.put(dragHandle, registeredDraggable);
     } catch (Exception ex) {
-      throw new RuntimeException(
-          "dragHandle must implement HasMouseDownHandlers to be draggable",
+      throw new RuntimeException("dragHandle must implement HasMouseDownHandlers to be draggable",
           ex);
     }
   }
@@ -325,8 +323,8 @@ class MouseDragHandler implements MouseMoveHandler, MouseDownHandler, MouseUpHan
   }
 
   private void startCapturing() {
-    capturingWidget.setPixelSize(
-        RootPanel.get().getOffsetWidth(), RootPanel.get().getOffsetHeight());
+    capturingWidget.setPixelSize(RootPanel.get().getOffsetWidth(),
+        RootPanel.get().getOffsetHeight());
     RootPanel.get().add(capturingWidget, 0, 0);
     DOM.setCapture(capturingWidget.getElement());
   }
