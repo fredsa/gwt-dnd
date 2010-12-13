4efc3be6eca68e4d2ccf49b21aee6c1e9bbdd682
Fred Sauer <fredsa@google.com>
2010-12-13 18:17:03 +0000

    Add {@value} to javadoc
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@1060 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/demo/com/allen_sauer/gwt/dnd/demo/client/util/GWTUtil.java
+++ DragDrop/demo/com/allen_sauer/gwt/dnd/demo/client/util/GWTUtil.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2009 Fred Sauer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -21,14 +21,15 @@ import com.allen_sauer.gwt.dnd.demo.client.DragDropDemo;
 public class GWTUtil {
 
   /**
-   * Base java package for demo client code, used by {@link #getClassAnchorHTML(Class)}.
+   * Base java package ({@value}) for demo client code, used by {@link #getClassAnchorHTML(Class)}.
    */
   public static final String DEMO_CLIENT_PACKAGE = GWTUtil.getPackageName(DragDropDemo.class);
 
   /**
-   * Base Subversion URL for DragDrop project.
+   * Base Subversion URL ({@value}) for DragDrop project.
    */
-  private static final String SUBVERSION_BROWSE_TRUNK = "http://code.google.com/p/gwt-dnd/source/browse/trunk/DragDrop/";
+  private static final String SUBVERSION_BROWSE_TRUNK =
+      "http://code.google.com/p/gwt-dnd/source/browse/trunk/DragDrop/";
 
   /**
    * Determine Subversion URL for provided class literal, to be used in HTML anchors.
