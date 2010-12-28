b29d6d92f1c4c3bd6bbc463651a43f71e1ce29be
Fred Sauer <fredsa@google.com>
2010-12-28 19:50:02 +0000

    Interactive run a test from the test launch
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@1080 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/test2/com/allen_sauer/gwt/dnd/test/client/DragDropTest.java
+++ DragDrop/test2/com/allen_sauer/gwt/dnd/test/client/DragDropTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2009 Fred Sauer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -67,5 +67,12 @@ public final class DragDropTest implements EntryPoint {
 
   public void onModuleLoad2() {
     RootPanel.get().add(new HTML("DragDropTest is in <b>" + getCompatMode() + "</b> mode."));
+    try {
+      MouseDragHandlerTest mouseDragHandlerTest = new MouseDragHandlerTest();
+      mouseDragHandlerTest.gwtSetUp();
+      mouseDragHandlerTest.testMouseDown();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
