6c9a1382d6428926eaaa4cf2ac155626952c0a4a
Fred Sauer <fredsa@google.com>
2011-06-03 21:05:30 +0000

    DeferredCommand -> Scheduler
    git-svn-id: https://gwt-dnd.googlecode.com/svn/trunk@1122 c7e6bd1f-9827-0410-b8b9-973c5d279602

--- DragDrop/demo/com/allen_sauer/gwt/dnd/demo/client/DragDropDemo.java
+++ DragDrop/demo/com/allen_sauer/gwt/dnd/demo/client/DragDropDemo.java
@@ -1,33 +1,17 @@
 /*
  * Copyright 2009 Fred Sauer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.allen_sauer.gwt.dnd.demo.client;
-
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
-import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.DeferredCommand;
-import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.Random;
-import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
 import com.allen_sauer.gwt.dnd.client.drop.BoundaryDropController;
@@ -45,6 +29,20 @@ import com.allen_sauer.gwt.dnd.demo.client.example.palette.PaletteExample;
 import com.allen_sauer.gwt.dnd.demo.client.example.puzzle.PuzzleExample;
 import com.allen_sauer.gwt.dnd.demo.client.example.resetcache.ResetCacheExample;
 import com.allen_sauer.gwt.dnd.demo.client.example.window.WindowExample;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * EntryPoint class for demonstrating and testing gwt-dnd.
@@ -184,7 +182,8 @@ public final class DragDropDemo implements EntryPoint {
       // select a random example
       examples.selectTab(Random.nextInt(examples.getTabCount() - 1));
     } else {
-      DeferredCommand.addCommand(new Command() {
+      Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+        @Override
         public void execute() {
           examples.selectTabByHistoryToken(initToken);
         }
