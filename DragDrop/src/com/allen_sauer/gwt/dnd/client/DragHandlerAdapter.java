8f8df91a2a62c02421b2107794bcf2ab6235f23b
Fred Sauer <fredsa@google.com>
2013-03-25 15:52:46 -0700

    unnecessary @SuppressWarnings("unused")

--- DragDrop/src/com/allen_sauer/gwt/dnd/client/DragHandlerAdapter.java
+++ DragDrop/src/com/allen_sauer/gwt/dnd/client/DragHandlerAdapter.java
@@ -27,12 +27,10 @@ public class DragHandlerAdapter implements DragHandler {
   public void onDragStart(DragStartEvent event) {
   }
 
-  @SuppressWarnings("unused")
   @Override
   public void onPreviewDragEnd(DragEndEvent event) throws VetoDragException {
   }
 
-  @SuppressWarnings("unused")
   @Override
   public void onPreviewDragStart(DragStartEvent event) throws VetoDragException {
   }
